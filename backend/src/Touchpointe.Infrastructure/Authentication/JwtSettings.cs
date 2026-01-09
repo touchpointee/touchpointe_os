be22d48e8045df48dcf9822de88a92461133b63a
ajmalj12 <ajmaljs2003@gmail.com>
2026-01-09 17:58:47 +0530

    Fix CS8852: Allow setting JWT Secret

--- backend/src/Touchpointe.Infrastructure/Authentication/JwtSettings.cs
+++ backend/src/Touchpointe.Infrastructure/Authentication/JwtSettings.cs
@@ -3,7 +3,7 @@ namespace Touchpointe.Infrastructure.Authentication
     public class JwtSettings
     {
         public const string SectionName = "JwtSettings";
-        public string Secret { get; init; } = null!;
+        public string Secret { get; set; } = null!;
         public int ExpiryMinutes { get; init; }
         public string Issuer { get; init; } = null!;
         public string Audience { get; init; } = null!;
