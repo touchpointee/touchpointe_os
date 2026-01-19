51ef7a5ac18768dfa5213849d82cf40299869920
ajmalj12 <ajmaljs2003@gmail.com>
2026-01-19 13:07:32 +0530

    Your commit message

--- backend/src/Touchpointe.Application/obj/Debug/net9.0/Touchpointe.Application.AssemblyInfo.cs
+++ backend/src/Touchpointe.Application/obj/Debug/net9.0/Touchpointe.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Touchpointe.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc7ff205c6dd7f32cd2edcea076eefc5416448c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6554610db56d6c459e6bde6ddf0a0b78f676aff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Touchpointe.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Touchpointe.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
